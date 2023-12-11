f590a34600474453713155c4d438868436c51fd9
ralphvirtucio <ralphvirtucio3@gmail.com>
2023-12-11 23:24:50 +0800

    Completed Desktop version

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-console.log('hello world')
 
+const disclosures = document.querySelectorAll('.disclosure')
+
+function toggleDisclosures() {
+  const isExpanded = this.getAttribute('aria-expanded') === 'true'
+  this.setAttribute('aria-expanded', !isExpanded)
+}
+
+
+disclosures.forEach(component => {
+  component.dataset.state = 'ready'
+
+  const btn = component.querySelector('.btn--disclosure')
+
+  btn.removeAttribute('disabled')
+  btn.removeAttribute('title')
+
+  btn.addEventListener('click', toggleDisclosures)
+})
